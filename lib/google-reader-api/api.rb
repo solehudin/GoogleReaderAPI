9aa18171f660bdc13f0a009099b5e3349a0af56d
dadan41 <dadan@41studio.com>
2013-01-17 16:24:18 +0700

    update Authorization type part 4

--- lib/google-reader-api/api.rb
+++ lib/google-reader-api/api.rb
@@ -66,10 +66,16 @@ module GoogleReaderApi
     def request(uri,request)
       # add the cookie to the http header
       #request.add_field('Authorization',"GoogleLogin auth=#{auth}")
-      request.add_field('Authorization',"OAuth auth=#{auth}")
-      res = Net::HTTP.start(uri.host,uri.port) do |http|
-        http.request(request)
-      end
+
+      # request.add_field('Authorization',"OAuth auth=#{auth}")
+      # res = Net::HTTP.start(uri.host,uri.port) do |http|
+      #   http.request(request)
+      # end
+
+      res = Typhoeus::Request.get(url.to_s, headers: {"Authorization" => "#{auth}"})
+
+      p '======================================='
+      p res.body
       # TODO: use better exception
       if res.code != '200'
         p res.body
